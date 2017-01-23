bbbe5f319818bb8e6c05ddf52458d1675f4fccfa
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-23 11:34:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
+[![Build Status](https://travis-ci.org/zrrrzzt/firebase-counter.svg?branch=master)](https://travis-ci.org/zrrrzzt/firebase-counter)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
 # firebase-counter
-Counter with firebase backend
+Counter with [firebase](https://firebase.google.com) backend
+
+## Installation
+
+```bash
+$ npm i firebase-counter --save
+```
+
+## Usage
+
+Add, subtract og lookup values for a specific key in firebase.
+- Uses ```value``` as key if no key supplied.
+- Defaults to 1 if no value is presented.
+- Supports promises and callbacks
+
+```JavaScript
+const fbc = require('firebase-counter')
+const counterOptions = {
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: '<your-database-url>',
+  appName: '<your-app-name>',
+  authEmail: '<your-auth-email>',
+  authPassword: '<your-auth-password>'
+}
+
+const counter = fbc(counterOptions)
+
+counter.add({'key': 'fishy', value: 10}).then(data => console.log(data))
+
+counter.subtract({'key': 'fishy', value: 5}).then(data => console.log(data))
+
+counter.lookup({'key': 'fishy'}).then(data => console.log(data))
+```
+
+## License
+[MIT](LICENSE)
+
+![alt text](https://robots.kebabstudios.party/firebase-counter.png "Robohash image of firebase-counter")
---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict'
+
+module.exports = (options) => {
+  const firebase = require('firebase')
+  const app = firebase.initializeApp({
+    apiKey: options.apiKey,
+    authDomain: options.authDomain,
+    databaseURL: options.databaseURL
+  }, options.appName)
+
+  if (options.authEmail && options.authPassword) {
+    app.auth().signInWithEmailAndPassword(options.authEmail, options.authPassword).catch(error => {
+      return error
+    })
+  }
+
+  const database = app.database()
+
+  const add = (args, callback) => {
+    return new Promise((resolve, reject) => {
+      const selectedKey = args.key || 'value'
+      const value = parseInt(args.value || 1, 10)
+      const valueRef = database.ref(selectedKey)
+      valueRef.transaction((currentVal) => {
+        return currentVal + value
+      }, (error, committed, snapshot) => {
+        if (error) {
+          if (callback) {
+            return callback(error, null)
+          }
+          reject(error)
+        } else {
+          const result = {key: selectedKey, value: snapshot.val()}
+          if (callback) {
+            return callback(null, result)
+          }
+          resolve(result)
+        }
+      })
+    })
+  }
+
+  const subtract = (args, callback) => {
+    return new Promise((resolve, reject) => {
+      const selectedKey = args.key || 'value'
+      const value = parseInt(args.value || 1, 10)
+      const valueRef = database.ref(selectedKey)
+      valueRef.transaction((currentVal) => {
+        return currentVal - value
+      }, (error, committed, snapshot) => {
+        if (error) {
+          if (callback) {
+            return callback(error, null)
+          }
+          reject(error)
+        } else {
+          const result = {key: selectedKey, value: snapshot.val()}
+          if (callback) {
+            return callback(null, result)
+          }
+          resolve(result)
+        }
+      })
+    })
+  }
+
+  const lookup = (args, callback) => {
+    return new Promise((resolve, reject) => {
+      const selectedKey = args.key || 'value'
+      database.ref(selectedKey).once('value').then((snapshot) => {
+        const value = snapshot.val()
+        const result = {key: selectedKey, value: value}
+        if (callback) {
+          return result
+        }
+        resolve(result)
+      }).catch((error) => {
+        reject(error)
+      })
+    })
+  }
+
+  return {
+    add: add,
+    subtract: subtract,
+    lookup: lookup
+  }
+}
